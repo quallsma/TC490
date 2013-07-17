b1a213c21c23504c62717c6e6042d29183bf107f
Marsalis Qualls <quallsma@msu.edu>
2013-07-17 16:26:12 -0400

    update

--- week2/resume/javascript/reume.js
+++ week2/resume/javascript/reume.js
@@ -3,15 +3,15 @@ $(document).ready(function(){
 	$('h3').mouseenter(function(){
 		$(this).css('background', '#CCFFFF');
 		$(this).css('opacity', '1.0');
-		$(this).animate({"padding-left": "+=320px"}, "slow");
-		$(this).animate({"font-size": "+=10px"}, "slow");
+		$(this).animate({
+			"padding-left": "+=320px"}, 'slow');
 	});
 	$('h3').mouseleave(function(){
+		$(this).stop();
 		$(this).css('background', 'none');
 		$(this).css('opacity', '0.5');
 		$(this).animate({
-			"padding-left": "-=320px",
-			"font-size": "-=10px"}, 'slow');
+			"padding-left": "-=320px"}, 'slow');
 
 	});
 });
